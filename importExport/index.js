7caacb64927237e62088191e75c815cbaf625af3
Saksham Katiyar <115263557+SakshamKatiy@users.noreply.github.com>
2024-01-27 11:15:17 +0530

    creating own module

---
+++ importExport/index.js
@@ -0,0 +1,4 @@
+const {add,sub,name} = require('./operator');
+console.log(add(5,5));
+console.log(name);
+console.log(sub(5,5));
---
+++ importExport/operator.js
@@ -0,0 +1,15 @@
+const add = (a,b) =>{
+    return a+b;
+
+};
+
+const sub = (a,b) =>{
+    return a-b;
+
+};
+
+const name = "Saksham"
+// module.exports.add=add;
+// module.exports.sub=sub;
+
+module.exports = {add , sub , name}
