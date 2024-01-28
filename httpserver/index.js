8d158583ba2639cfa613b3094712b8ac0ea71aec
Saksham Katiyar <115263557+SakshamKatiy@users.noreply.github.com>
2024-01-28 12:39:43 +0530

    create api

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,6 +1,10 @@
 const http  = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req,res)=>{
+    const data = fs.readFileSync(`${__dirname}/UserApi/userapi.json`,'utf-8');
+    const objdata = JSON.parse(data);
+    
     if(req.url == "/"){
         res.end("Hello Home is responding");
 
@@ -11,11 +15,21 @@ const server = http.createServer((req,res)=>{
     else if(req.url == "/contact"){
         res.end("this is contact us page");
 
-    }else{
+    }
+    
+    else if(req.url == "/userapi"){
+        res.writeHead(200, {"Content-type":"application/json"});
+        console.log(data);
+        res.end(objdata.name);
+        
+        }
+    
+    else{
         res.writeHead(404, {"Content-type":"text/html"});
         res.end("<h1>404 error</h1>");
     }
 });
+
 server.listen(8000,"127.0.0.1",()=>{
     console.log("working")
 })
